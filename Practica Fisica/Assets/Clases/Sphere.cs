b06cce4168ade8d248e829d365a1603ca68b1909
Eduard <edw_generate@hotmail.com>
2017-12-05 17:08:08 +0100

    Material + Quat

--- Practica Fisica/Assets/Clases/Sphere.cs
+++ Practica Fisica/Assets/Clases/Sphere.cs
@@ -1,22 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Sphere : MonoBehaviour{
+public class Sphere : MonoBehaviour {
 
+    //Movement variables
     [SerializeField]
     private Vec3 position;
     private Vec3 velocity;
     private Vec3 linearMomentum;
+
+    //Rotation Variables
+    private Quat rotation;
+    private Vec3 torque;
+    private Vec3 angularMomentum;
+    private Vec3 angularVelocity;
+    
+    //World Variables
     private Vec3 gravity;
+
+    //Object variables
     [SerializeField]
     private float radius;
     [SerializeField]
     private float mass;
+
+    //Inertia tensor
     private Mat3 inertiaTensor;
+    private Mat3 inverseInertiaTensor;
 
     public void Start()
     {
-        inertiaTensor = new Mat3( new float[,]{ { (2/5)*mass*radius*radius, 0, 0},
+        inertiaTensor = new Mat3(new float[,]{ { (2/5)*mass*radius*radius, 0, 0},
                                                 { 0, (2/5)*mass*radius*radius, 0},
                                                 { 0, 0, (2/5)*mass*radius*radius} });
         position = new Vec3();
@@ -44,16 +58,36 @@ public class Sphere : MonoBehaviour{
     }
     public void Update()
     {
-        float dt = Time.deltaTime;
-        //Update the linear momentum for the gravity force which is applied each frame
-        linearMomentum = linearMomentum + (gravity * dt);
-        velocity = linearMomentum / mass;
-        position = position + velocity * dt;
 
-        this.transform.position = new Vector3(position.x, position.y, position.z);
+        //Update the object's position
+        EulerStep(Time.deltaTime);
+
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("Space Pressed");
+            AddForce(new Vec3(0f, 10f, 0f), new Vec3(0.25f, 0f, 0f));
+        }
     }
-    public void AddForce(Vec3 force)
+    public void AddForce(Vec3 force, Vec3 applicationPoint)
     {
-        linearMomentum = linearMomentum + force; // no dt because it's an impulse force
+        //The application point is expressed in the sphere local coordinates
+        linearMomentum += force; // no dt because it's an impulse force
+        torque = Vec3.crossProduct(force, applicationPoint);
+        angularMomentum += torque;
+    }
+
+    public void EulerStep(float dt)
+    {
+        //The object is moved here
+        linearMomentum += (gravity * mass * dt);
+        velocity = linearMomentum / mass;
+        position += velocity * dt;
+ 
+        this.transform.position = new Vector3(position.x, position.y, position.z);
+
+        //The object is rotated here
+        Mat3 tempI;
+        //angularVelocity = tempI * angularMomentum * dt;
+        
     }
 }
