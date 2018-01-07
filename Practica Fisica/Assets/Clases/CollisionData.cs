7963d9b014934c07b823edbb0157667992c792f2
dPerez015 <davidperezgallego@enti.cat>
2018-01-07 22:55:22 +0100

    Guardadas las referencias a los obj en el collisionData

--- Practica Fisica/Assets/Clases/CollisionData.cs
+++ Practica Fisica/Assets/Clases/CollisionData.cs
@@ -6,12 +6,16 @@ public class CollisionData
     private Vec3 contact_normal;
     private Vec3 contact_point;
     private float penetration_depth;
+    private PhysicalObject[] objects;
 
-    public CollisionData (Vec3 normal, Vec3 point, float depth)
+    public CollisionData (Vec3 normal, Vec3 point, float depth, PhysicalObject ob1, PhysicalObject ob2)
     {
         contact_normal = normal;
         contact_point = point;
         penetration_depth = depth;
+        objects = new PhysicalObject[2];
+        objects[0] = ob1;
+        objects[1] = ob2;
     }
     public Vec3 GetContactNormal() { return contact_normal; }
     public Vec3 GetContactPoint() { return contact_point; }
